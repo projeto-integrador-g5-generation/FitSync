4020d5384c8da6a5d419bfe05b21eb89cfc05aca
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-01-22 12:19:28 -0300

    :sparkles: feat: Implementação do crud de Usuario

---
+++ src/usuario/entities/usuario.entity.ts
@@ -0,0 +1,41 @@
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator"
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+
+@Entity({name: "tb_usuarios"})
+export class Usuario {
+
+    @PrimaryGeneratedColumn() 
+    id: number
+
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false}) 
+    nome: string
+
+    @IsNotEmpty()
+    @Column({type: 'date', nullable: false})
+    data_nascimento: Date
+
+    @IsNotEmpty()
+    @Column('decimal', {precision: 5, scale: 2, nullable: true})
+    peso: number
+
+    @IsNotEmpty()
+    @Column('decimal', {precision: 3, scale: 2, nullable: true})
+    altura: number
+
+    @IsEmail()
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false })
+    usuario: string
+
+    @MinLength(8)
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false }) 
+    senha: string
+
+
+    @Column({length: 5000 }) 
+    foto: string
+
+}
